59a6a76c071741f313477dbaeab0397e2ee519dc
DongjinSon9 <dongj1358@naver.com>
2024-02-23 14:55:21 +0900

    메인화면 본문 ex 제작 중

--- JS/js.js
+++ JS/js.js
@@ -2,12 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
     var profilePic = document.getElementById("profile-pic");
     var dropdownContent = document.getElementById("dropdown-content");
 
-    // 프로필 이미지를 클릭할 때 드롭다운 메뉴를 토글합니다.
+    // 프로필 이미지를 클릭할 때 드롭다운 메뉴를 토글합니다. * 프로필 이미지 클릭하면 드롭다운컨텐츠의 클래스에 show를 추가한다.
     profilePic.addEventListener("click", function () {
         dropdownContent.classList.toggle("show");
     });
 
     // 화면 어느 곳을 클릭하더라도 드롭다운 메뉴가 열려있다면 닫습니다.
+    // 클릭한 타겟이 프로필 이미지가 아니라면 드롭다운컨텐츠의 클래스 리스트에 show가 있는지 확인하고 있으면 show를 지운다.
     window.addEventListener("click", function (event) {
         if (!event.target.matches("#profile-pic")) {
             if (dropdownContent.classList.contains("show")) {
